49f0f508cd7c8cc2f1adf24bae6698ef74a655c9
Denis <korbakovd@gmail.com>
2025-07-15 22:12:30 +0300

    fix: update str verify on type and delete extra code

--- internal/component/input/input.go
+++ internal/component/input/input.go
@@ -2,36 +2,23 @@ package input
 
 import (
 	"errors"
-	"fmt"
 	"reflect"
 	"strconv"
-	"strings"
 
-	"github.com/charmbracelet/bubbles/cursor"
-	"github.com/charmbracelet/bubbles/textinput"
 	tea "github.com/charmbracelet/bubbletea"
-	"github.com/charmbracelet/lipgloss"
-)
-
-var (
-	focusedStyle        = lipgloss.NewStyle().Foreground(lipgloss.Color("205"))
-	blurredStyle        = lipgloss.NewStyle().Foreground(lipgloss.Color("240"))
-	cursorStyle         = focusedStyle
-	noStyle             = lipgloss.NewStyle()
-	helpStyle           = blurredStyle
-	cursorModeHelpStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("244"))
-
-	focusedButton = focusedStyle.Render("[ Submit ]")
-	blurredButton = fmt.Sprintf("[ %s ]", blurredStyle.Render("Submit"))
-
-	errInitInput        = errors.New("input initialization failed")
-	errInvalidModelType = errors.New("invalid model type")
 )
 
 const (
-	ChatLimit  = 32
-	Width      = 20
-	HiddenChar = '*'
+	fieldNumberAlias = iota
+	fieldNumberLogin
+	fieldNumberPassword
+	fieldNumberPort
+	fieldNumberPrivateKey
+)
+
+var (
+	errInitInput        = errors.New("input initialization failed")
+	errInvalidModelType = errors.New("invalid model type")
 )
 
 type Fields struct {
@@ -44,7 +31,6 @@ type Fields struct {
 
 func (f *Fields) count() int {
 	fieldsType := reflect.TypeOf(*f)
-
 	return fieldsType.NumField()
 }
 
@@ -63,156 +49,7 @@ func (f *Fields) getNameByNumber(number int) string {
 	return typ.Field(number).Name
 }
 
-type model struct {
-	focusIndex int
-	inputs     []textinput.Model
-	cursorMode cursor.Mode
-}
-
-func (m model) Init() tea.Cmd {
-	return textinput.Blink
-}
-
-func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	switch msg := msg.(type) {
-	case tea.KeyMsg:
-		switch msg.String() {
-		case "ctrl+c", "esc":
-			return m, tea.Quit
-
-		// Change cursor mode
-		case "ctrl+r":
-			m.cursorMode++
-			if m.cursorMode > cursor.CursorHide {
-				m.cursorMode = cursor.CursorBlink
-			}
-			cmds := make([]tea.Cmd, len(m.inputs))
-			for i := range m.inputs {
-				cmds[i] = m.inputs[i].Cursor.SetMode(m.cursorMode)
-			}
-			return m, tea.Batch(cmds...)
-
-		// Set focus to next input
-		case "tab", "shift+tab", "enter", "up", "down":
-			s := msg.String()
-
-			// Did the user press enter while the submit button was focused?
-			// If so, exit.
-			if s == "enter" && m.focusIndex == len(m.inputs) {
-				return m, tea.Quit
-			}
-
-			// Cycle indexes
-			if s == "up" || s == "shift+tab" {
-				m.focusIndex--
-			} else {
-				m.focusIndex++
-			}
-
-			if m.focusIndex > len(m.inputs) {
-				m.focusIndex = 0
-			} else if m.focusIndex < 0 {
-				m.focusIndex = len(m.inputs)
-			}
-
-			cmds := make([]tea.Cmd, len(m.inputs))
-			for i := 0; i <= len(m.inputs)-1; i++ {
-				if i == m.focusIndex {
-					// Set focused state
-					cmds[i] = m.inputs[i].Focus()
-					m.inputs[i].PromptStyle = focusedStyle
-					m.inputs[i].TextStyle = focusedStyle
-					continue
-				}
-				// Remove focused state
-				m.inputs[i].Blur()
-				m.inputs[i].PromptStyle = noStyle
-				m.inputs[i].TextStyle = noStyle
-			}
-
-			return m, tea.Batch(cmds...)
-		}
-	}
-
-	// Handle character input and blinking
-	cmd := m.updateInputs(msg)
-
-	return m, cmd
-}
-
-func (m model) View() string {
-	var b strings.Builder
-
-	for i := range m.inputs {
-		b.WriteString(m.inputs[i].View())
-		if i < len(m.inputs)-1 {
-			b.WriteRune('\n')
-		}
-	}
-
-	button := &blurredButton
-	if m.focusIndex == len(m.inputs) {
-		button = &focusedButton
-	}
-	fmt.Fprintf(&b, "\n\n%s\n\n", *button)
-
-	b.WriteString(helpStyle.Render("cursor mode is "))
-	b.WriteString(cursorModeHelpStyle.Render(m.cursorMode.String()))
-	b.WriteString(helpStyle.Render(" (ctrl+r to change style)"))
-
-	return b.String()
-}
-
-func (m *model) updateInputs(msg tea.Msg) tea.Cmd {
-	cmds := make([]tea.Cmd, len(m.inputs))
-
-	// Only text inputs with Focus() set will respond, so it's safe to simply
-	// update all of them here without any further logic.
-	for i := range m.inputs {
-		m.inputs[i], cmds[i] = m.inputs[i].Update(msg)
-	}
-
-	return tea.Batch(cmds...)
-}
-
-func initModel() model {
-	fileds := &Fields{}
-	m := model{
-		inputs: make([]textinput.Model, fileds.count()),
-	}
-
-	var t textinput.Model
-	for i := range m.inputs {
-		t = textinput.New()
-		t.Cursor.Style = cursorStyle
-		t.CharLimit = ChatLimit
-		t.Width = Width
-
-		switch i {
-		case 0:
-			t.Focus()
-			t.Placeholder = fileds.getNameByNumber(0)
-			t.PromptStyle = focusedStyle
-			t.TextStyle = focusedStyle
-		case 1:
-			t.Placeholder = fileds.getNameByNumber(1)
-		case 2:
-			t.Placeholder = fileds.getNameByNumber(2)
-			t.EchoMode = textinput.EchoPassword
-			t.EchoCharacter = HiddenChar
-		case 3:
-			t.Placeholder = fileds.getNameByNumber(3)
-		case 4:
-			t.Placeholder = fileds.getNameByNumber(4)
-		}
-
-		m.inputs[i] = t
-	}
-
-	return m
-}
-
-func Init() (*Fields, error) {
+func Run() (*Fields, error) {
 	returnedModel, err := tea.NewProgram(initModel()).Run()
 	if err != nil {
 		return nil, errInitInput
@@ -223,16 +60,16 @@ func Init() (*Fields, error) {
 		return nil, errInvalidModelType
 	}
 
-	port, err := strconv.Atoi(currentModel.inputs[3].Value())
+	port, err := strconv.Atoi(currentModel.inputs[fieldNumberPort].Value())
 	if err != nil {
 		return nil, err
 	}
 
 	return &Fields{
-		Alias:      currentModel.inputs[0].Value(),
-		Login:      currentModel.inputs[1].Value(),
-		Password:   currentModel.inputs[2].Value(),
+		Alias:      currentModel.inputs[fieldNumberAlias].Value(),
+		Login:      currentModel.inputs[fieldNumberLogin].Value(),
+		Password:   currentModel.inputs[fieldNumberPassword].Value(),
 		Port:       port,
-		PrivateKey: currentModel.inputs[4].Value(),
+		PrivateKey: currentModel.inputs[fieldNumberPrivateKey].Value(),
 	}, nil
 }
