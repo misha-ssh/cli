b55f2413c0ea90e1acc57aef5521bee44b6e3df1
Denis <korbakovd@gmail.com>
2025-07-15 14:25:56 +0300

    feat: add logic for create connection

--- internal/command/create.go
+++ internal/command/create.go
@@ -1,9 +1,12 @@
 package command
 
 import (
-	"fmt"
+	"time"
+
 	"github.com/misha-ssh/cli/configs/envconst"
 	"github.com/misha-ssh/cli/internal/component/input"
+	"github.com/misha-ssh/kernel/pkg/connect"
+	"github.com/misha-ssh/kernel/pkg/kernel"
 	"github.com/spf13/cobra"
 )
 
@@ -18,7 +21,25 @@ var createCmd = &cobra.Command{
 			return err
 		}
 
-		fmt.Println(fields)
+		currentTime := time.Now()
+
+		connection := &connect.Connect{
+			Alias:     fields.Alias,
+			Login:     fields.Login,
+			Password:  fields.Password,
+			CreatedAt: currentTime.Format("2006.01.02 15:04:05"),
+			UpdatedAt: currentTime.Format("2006.01.02 15:04:05"),
+			Type:      connect.TypeSSH,
+			SshOptions: &connect.SshOptions{
+				Port:       fields.Port,
+				PrivateKey: fields.PrivateKey,
+			},
+		}
+
+		err = kernel.Create(connection)
+		if err != nil {
+			return err
+		}
 
 		return nil
 	},
