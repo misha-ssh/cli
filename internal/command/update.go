f937179648b06320d284101d8ae269544e680428
Denis <korbakovd@gmail.com>
2025-07-30 00:53:34 +0300

    feat: add test example update connection

--- internal/command/update.go
+++ internal/command/update.go
@@ -1,18 +1,32 @@
 package command
 
 import (
-	"errors"
+	"fmt"
+	"github.com/misha-ssh/cli/internal/form/list"
+	"github.com/misha-ssh/kernel/pkg/kernel"
 
 	"github.com/misha-ssh/cli/configs/envconst"
 	"github.com/spf13/cobra"
 )
 
-// updateCmd Command for update connection
+// updateCmd Command for update create
 var updateCmd = &cobra.Command{
 	Use:   envconst.UseUpdateCmd,
 	Short: envconst.ShortUpdateCmd,
 	Long:  envconst.LongUpdateCmd,
 	RunE: func(cmd *cobra.Command, args []string) error {
-		return errors.New("update")
+		connections, err := kernel.List()
+		if err != nil {
+			return err
+		}
+
+		selectedConnection, err := list.Run(connections)
+		if err != nil {
+			return err
+		}
+
+		fmt.Println(selectedConnection)
+
+		return nil
 	},
 }
