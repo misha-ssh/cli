8ed717b6fab06a87fa501f6448f15ca47c0d27d8
Denis <korbakovd@gmail.com>
2025-07-29 21:03:17 +0300

    feat: add errors

--- internal/command/create.go
+++ internal/command/create.go
@@ -1,13 +1,21 @@
 package command
 
 import (
+	"errors"
+	"strconv"
 	"time"
 
 	"github.com/misha-ssh/cli/configs/envconst"
-	"github.com/misha-ssh/cli/internal/component/input"
+	"github.com/misha-ssh/cli/internal/component/textinput"
 	"github.com/misha-ssh/kernel/pkg/connect"
 	"github.com/misha-ssh/kernel/pkg/kernel"
 	"github.com/spf13/cobra"
+)
+
+var (
+	errRunTextInput     = errors.New("error run text input")
+	errConvertPort      = errors.New("error convert port")
+	errCreateConnection = errors.New("error create connection")
 )
 
 // createCmd Command for create connection
@@ -16,12 +24,17 @@ var createCmd = &cobra.Command{
 	Short: envconst.ShortCreateCmd,
 	Long:  envconst.LongCreateCmd,
 	RunE: func(cmd *cobra.Command, _ []string) error {
-		fields, err := input.Run()
+		fields, err := textinput.Run()
 		if err != nil {
-			return err
+			return errRunTextInput
 		}
 
 		currentTime := time.Now()
+
+		port, err := strconv.Atoi(fields.Port)
+		if err != nil {
+			return errConvertPort
+		}
 
 		connection := &connect.Connect{
 			Alias:     fields.Alias,
@@ -31,14 +44,14 @@ var createCmd = &cobra.Command{
 			UpdatedAt: currentTime.Format("2006.01.02 15:04:05"),
 			Type:      connect.TypeSSH,
 			SshOptions: &connect.SshOptions{
-				Port:       fields.Port,
+				Port:       port,
 				PrivateKey: fields.PrivateKey,
 			},
 		}
 
 		err = kernel.Create(connection)
 		if err != nil {
-			return err
+			return errCreateConnection
 		}
 
 		return nil
