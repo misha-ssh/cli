8fb411b7e4b91554328957fe29cdf00b17c40624
Denis <korbakovd@gmail.com>
2025-07-31 00:02:28 +0300

    fix: update form create connection

--- internal/form/create/form.go
+++ internal/form/create/form.go
@@ -11,6 +11,8 @@ const (
 )
 
 func Run() (*Fields, error) {
+	var authPassConfirm bool
+
 	homedir, err := os.UserHomeDir()
 	if err != nil {
 		return nil, err
@@ -35,25 +37,37 @@ func Run() (*Fields, error) {
 				Value(&fields.Login),
 
 			huh.NewInput().
-				Title("Password").
-				EchoMode(huh.EchoModePassword).
-				Description("Password to connect to a remote machine").
-				Validate(huh.ValidateNotEmpty()).
-				Value(&fields.Password),
-
-			huh.NewInput().
 				Title("Port").
-				Description("port number to connect to a remote machine").
+				Description("Port number to connect to a remote machine").
 				Validate(portValidate).
 				Value(&fields.Port),
 
+			huh.NewConfirm().
+				Title("Authentication").
+				Description("Choice method authentication").
+				Affirmative("Password").
+				Negative("Private key").
+				Value(&authPassConfirm),
+		),
+		huh.NewGroup(
+			huh.NewInput().
+				Title("Password").
+				EchoMode(huh.EchoModePassword).
+				Description("Password to connect to a remote machine").
+				Value(&fields.Password),
+		).WithHideFunc(func() bool {
+			return !authPassConfirm
+		}),
+		huh.NewGroup(
 			huh.NewFilePicker().
 				Title("PrivateKey").
 				Description("select file with private key").
 				CurrentDirectory(homedir).
 				Validate(privateKeyValidate).
 				Value(&fields.PrivateKey),
-		),
+		).WithHideFunc(func() bool {
+			return authPassConfirm
+		}),
 	).WithShowHelp(true).Run()
 
 	return fields, nil
