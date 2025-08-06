8df30b79dd998a03ecfc0f813d50854147c8c021
Denis <korbakovd@gmail.com>
2025-08-06 21:07:54 +0300

    feat: add base logic for update connection

--- internal/command/update.go
+++ internal/command/update.go
@@ -1,12 +1,24 @@
 package command
 
 import (
-	"fmt"
-	"github.com/misha-ssh/cli/internal/form/list"
-	"github.com/misha-ssh/kernel/pkg/kernel"
+	"errors"
+	"strconv"
+	"time"
+
+	updateForm "github.com/misha-ssh/cli/internal/form/update"
 
 	"github.com/misha-ssh/cli/configs/envconst"
+	"github.com/misha-ssh/cli/internal/form/list"
+	"github.com/misha-ssh/cli/internal/output"
+	"github.com/misha-ssh/kernel/pkg/connect"
+	"github.com/misha-ssh/kernel/pkg/kernel"
 	"github.com/spf13/cobra"
+)
+
+var (
+	successUpdateConnection = "success update connection"
+
+	errNotFoundConnection = errors.New("not found connections")
 )
 
 // updateCmd Command for update create
@@ -20,13 +32,43 @@ var updateCmd = &cobra.Command{
 			return err
 		}
 
-		selectedConnection, err := list.Run(connections)
+		selectedConn, err := list.Run(connections)
 		if err != nil {
 			return err
 		}
 
-		fmt.Println(selectedConnection)
+		for _, conn := range connections.Connects {
+			if conn.Alias == selectedConn.Alias {
+				fields, err := updateForm.Run(&conn)
 
-		return nil
+				port, err := strconv.Atoi(fields.Port)
+				if err != nil {
+					return errConvertPort
+				}
+
+				updatedConnection := &connect.Connect{
+					Alias:     fields.Alias,
+					Login:     fields.Login,
+					Password:  fields.Password,
+					UpdatedAt: time.Now().Format("2006.01.02 15:04:05"),
+					Type:      connect.TypeSSH,
+					SshOptions: &connect.SshOptions{
+						Port:       port,
+						PrivateKey: fields.PrivateKey,
+					},
+				}
+
+				err = kernel.Update(updatedConnection, selectedConn.Alias)
+				if err != nil {
+					return err
+				}
+
+				output.Success(successUpdateConnection + " - " + selectedConn.Alias)
+
+				return nil
+			}
+		}
+
+		return errNotFoundConnection
 	},
 }
