24fcf6a047bdf1b07a5186da748ba0c39e6e5d01
Denis <korbakovd@gmail.com>
2025-08-09 20:53:51 +0300

    feat: add logic for delete connection

--- internal/command/delete.go
+++ internal/command/delete.go
@@ -1,8 +1,18 @@
 package command
 
 import (
+	"errors"
 	"github.com/misha-ssh/cli/configs/envconst"
+	"github.com/misha-ssh/cli/internal/component/list"
+	"github.com/misha-ssh/cli/internal/component/output"
+	"github.com/misha-ssh/kernel/pkg/kernel"
 	"github.com/spf13/cobra"
+)
+
+var (
+	successDeleteConnection = "success delete connection"
+
+	errDeleteNotFoundConnection = errors.New("connection not found")
 )
 
 // deleteCmd Command for delete connection
@@ -11,6 +21,29 @@ var deleteCmd = &cobra.Command{
 	Short: envconst.ShortDeleteCmd,
 	Long:  envconst.LongDeleteCmd,
 	RunE: func(cmd *cobra.Command, args []string) error {
-		return nil
+		connections, err := kernel.List()
+		if err != nil {
+			return err
+		}
+
+		selectedConn, err := list.Run(connections)
+		if err != nil {
+			return err
+		}
+
+		for _, conn := range connections.Connects {
+			if conn.Alias == selectedConn.Alias {
+				err = kernel.Delete(&conn)
+				if err != nil {
+					return err
+				}
+
+				output.Success(successDeleteConnection + " - " + selectedConn.Alias)
+
+				return nil
+			}
+		}
+
+		return errDeleteNotFoundConnection
 	},
 }
