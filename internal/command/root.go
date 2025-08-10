03a0b798f216d656f6861ab259c24bb04776de35
Denis <korbakovd@gmail.com>
2025-08-11 02:11:39 +0300

    fix: delete extra use

--- internal/command/root.go
+++ internal/command/root.go
@@ -2,18 +2,12 @@ package command
 
 import (
 	"context"
-	"errors"
 	"os"
 
 	"github.com/charmbracelet/fang"
 	"github.com/misha-ssh/cli/configs/envconst"
-	"github.com/misha-ssh/cli/internal/component/output"
 	"github.com/misha-ssh/cli/internal/version"
 	"github.com/spf13/cobra"
-)
-
-var (
-	errRunApp = errors.New("error running app")
 )
 
 // Run Start app with cobra cmd
@@ -37,7 +31,6 @@ func Run() {
 		cmd,
 		fang.WithVersion(version.Get()),
 	); err != nil {
-		output.Error(errRunApp.Error())
 		os.Exit(1)
 	}
 }
