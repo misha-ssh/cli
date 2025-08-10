8a7371337efe137f9870cfe0bb15cf2e7b228bc8
Denis <korbakovd@gmail.com>
2025-08-10 11:00:29 +0300

    feat: add logic connection

--- internal/command/connect.go
+++ internal/command/connect.go
@@ -1,7 +1,8 @@
 package command
 
 import (
-	"errors"
+	"github.com/misha-ssh/cli/internal/component/list"
+	"github.com/misha-ssh/kernel/pkg/kernel"
 
 	"github.com/misha-ssh/cli/configs/envconst"
 	"github.com/spf13/cobra"
@@ -13,6 +14,27 @@ var connectCmd = &cobra.Command{
 	Short: envconst.ShortConnectCmd,
 	Long:  envconst.LongConnectCmd,
 	RunE: func(cmd *cobra.Command, args []string) error {
-		return errors.New("connect")
+		connections, err := kernel.List()
+		if err != nil {
+			return err
+		}
+
+		selectedConn, err := list.Run(connections)
+		if err != nil {
+			return err
+		}
+
+		for _, conn := range connections.Connects {
+			if conn.Alias == selectedConn.Alias {
+				err = kernel.Connect(&conn)
+				if err != nil {
+					return err
+				}
+
+				return nil
+			}
+		}
+
+		return errDeleteNotFoundConnection
 	},
 }
