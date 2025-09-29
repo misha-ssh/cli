e89ebf1fc444b4a51833adfccacfe29c6fb2cd9e
Denis <korbakovd@gmail.com>
2025-09-29 18:53:21 +0300

    feat: add validate for local file

---
+++ internal/component/cp/validate.go
@@ -0,0 +1,25 @@
+package cp
+
+import (
+	"errors"
+	"os"
+	"strings"
+)
+
+var (
+	errFileNotExist = errors.New("file not exists")
+)
+
+// localFileValidate validate on exists file
+func localFileValidate(filename string) error {
+	if strings.TrimSpace(filename) == "" || filename == "" {
+		return nil
+	}
+
+	_, err := os.Stat(filename)
+	if err != nil {
+		return errFileNotExist
+	}
+
+	return nil
+}
